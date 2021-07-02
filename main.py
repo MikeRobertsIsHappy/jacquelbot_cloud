7adefd85b42e511a92229e2bae21f3fb4271b191
ListeningJackal <MikeRobertsIsHappy@gmail.com>
2021-07-01 22:56:46 -0700

    More complex flask app with index and css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
+# Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,32 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START gae_flex_quickstart]
-import logging
+# [START gae_python38_render_template]
+# [START gae_python3_render_template]
+import datetime
 
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
 
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+    return render_template('index.html', times=dummy_times)
 
 
 if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_flex_quickstart]
+# [END gae_python3_render_template]
+# [END gae_python38_render_template]
